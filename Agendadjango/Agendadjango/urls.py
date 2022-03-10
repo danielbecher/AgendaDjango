eb1cc18a7af4bec8fae1a34a48d50ac4727db772
Daniel <daniel@becher.com.br>
2022-03-10 11:03:46 -0300

    Mais alguns ajustes no projeto Agenda Django

--- Agendadjango/Agendadjango/urls.py
+++ Agendadjango/Agendadjango/urls.py
@@ -1,4 +1,4 @@
-"""agenda URL Configuration
+"""Agendadjango URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
